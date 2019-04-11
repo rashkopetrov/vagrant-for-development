203e5cea0c6b8a211270e9afae77d9668d2ad8b7
Rashko Petrov <brutalenemy666@gmail.com>
2019-04-11 21:34:25 +0300

    Allow specifying the files user, group and mode

--- lib/devboxfiles.rb
+++ lib/devboxfiles.rb
@@ -11,9 +11,7 @@ class DevBoxFiles
     end
 
     files.each do |file_data|
-      file_path = File.expand_path(file_data['host'])
-
-      DevBoxFiles.validate(file_path)
+      DevBoxFiles.validate(file_data)
 
       DevBoxFiles.sync_file(file_data)
     end
@@ -21,11 +19,26 @@ class DevBoxFiles
 
   protected
   def self.sync_file(file_data)
-    DevBoxConf.config.vm.provision "file", source: file_data['host'], destination: file_data['guest'], run: file_data['sync']
+    sync = file_data['sync'] ||= "once"
+    file_content = File.read(File.expand_path(file_data['host']))
+    destination = file_data['guest']
+
+    options = file_data['options'] ||= {}
+    user = options['user'] ||= 'vagrant'
+    group = options['group'] ||= 'vagrant'
+    mode = options['mode'] ||= 644
+
+    DevBoxConf.config.vm.provision 'shell', run: sync do |s|
+      s.privileged = true
+      s.inline = "echo \"$1\" > $2 && chown $3:$4 $2 && chmod $5 $2"
+      s.args = [file_content, destination, user, group, mode]
+    end
   end
 
   protected
-  def self.validate(file_path)
+  def self.validate(file_data)
+    file_path = File.expand_path(file_data['host'])
+
     return if File.exist? file_path
 
     abort 'file not found: ' + file_path
