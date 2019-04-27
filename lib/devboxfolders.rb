7ecbe18905c0ae9c9e207dc16f580509c3bf3107
Rashko Petrov <brutalenemy666@gmail.com>
2019-04-27 16:35:14 +0300

    Fix the directory mounting options

--- lib/devboxfolders.rb
+++ lib/devboxfolders.rb
@@ -30,6 +30,8 @@ class DevBoxFolders
       mount_opts = folder_data['mount_options'] ? folder_data['mount_options'] : ['vers=3.02', 'mfsymlinks']
 
       smb_creds = {'smb_host': folder_data['smb_host'], 'smb_username': folder_data['smb_username'], 'smb_password': folder_data['smb_password']}
+    elsif folder_data['mount_options']
+      mount_opts = folder_data['mount_options']
     end
 
     # For b/w compatibility keep separate 'mount_opts', but merge with options
@@ -38,7 +40,7 @@ class DevBoxFolders
     # Double-splat (**) operator only works with symbol keys, so convert
     options.keys.each{|k| options[k.to_sym] = options.delete(k) }
 
-    DevBoxConf.config.vm.synced_folder folder_data['map'], folder_data['to'], type: folder_data['type'] ||= nil, **options
+    DevBoxConf.config.vm.synced_folder folder_data['map'], folder_data['to'], type: folder_data['type'] ||= nil, owner: folder_data['owner'] ||= 'vagrant', group: folder_data['type'] ||= 'vagrant', **options
 
     # Bindfs support to fix shared folder (NFS) permission issue on Mac
     if folder_data['type'] == 'nfs' && Vagrant.has_plugin?('vagrant-bindfs')
