87860378f13fa92c400f8f23f58d38ebbd77e5aa
Rashko Petrov <brutalenemy666@gmail.com>
2019-04-29 18:03:23 +0300

    Apache2 + PHP-FPM by default

--- resources/templates/apache2/default.tpl
+++ resources/templates/apache2/default.tpl
@@ -4,6 +4,8 @@
     ServerAdmin webmaster@localhost
     DocumentRoot {{SERVER_ROOT}}
 
+    DirectoryIndex index.php index.html index.htm index.shtml
+
     <Directory "{{SERVER_ROOT}}">
         Options FollowSymLinks
         AllowOverride All
@@ -11,6 +13,14 @@
         Order allow,deny
         Allow from all
     </Directory>
+
+    <Proxy "unix:/var/run/php/php7.2-fpm.sock|fcgi://php-fpm">
+        ProxySet disablereuse=off
+    </Proxy>
+
+    <FilesMatch \.php$>
+        SetHandler proxy:fcgi://php-fpm
+    </FilesMatch>
 </VirtualHost>
 
 # vim: syntax=apache ts=4 sw=4 sts=4 sr noet
--- scripts/installers/install-apache2.sh
+++ scripts/installers/install-apache2.sh
@@ -12,17 +12,19 @@ touch /home/vagrant/.installed-apache2
 usermod -a -G vagrant www-data
 apt-get -y  --no-install-recommends install apache2
 apt-get -y install libapache2-mod-fcgid libapache2-mod-php7.0 libapache2-mod-php7.1 libapache2-mod-php7.2 libapache2-mod-php7.3
-a2enmod alias
-a2enmod authz_host
-a2enmod deflate
-a2enmod dir
-a2enmod expires
-a2enmod headers
-a2enmod mime
-a2enmod rewrite
-a2enmod autoindex
-a2enmod negotiation
-a2enmod setenvif
+a2enmod proxy \
+    proxy_fcgi \
+    alias \
+    authz_host \
+    deflate \
+    dir \
+    expires \
+    headers \
+    mime \
+    rewrite \
+    autoindex \
+    negotiation \
+    setenvif
 rm -f /etc/apache2/sites-enabled/*
 rm -f /etc/apache2/sites-available/*
 service apache2 restart
